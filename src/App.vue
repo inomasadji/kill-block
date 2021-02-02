8858c0575c5c3a5cf28447cbb3428cbc412f85a4
Sergei Rodionov <inomasadji@gmail.com>
2021-02-02 19:25:18 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: monospace;
+}
+</style>
---
+++ src/components/BlockContainer.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "BlockContainer"
+}
+</script>
+
+<style scoped>
+
+</style>
